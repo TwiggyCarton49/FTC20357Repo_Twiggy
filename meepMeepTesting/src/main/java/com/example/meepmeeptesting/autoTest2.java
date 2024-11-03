f06f65d90dfd772118cc056ad7980219345356e0
TwiggyCarton49 <zhavard3@gmail.com>
2024-11-03 18:50:32 -0500

    FTC is life

--- meepMeepTesting/src/main/java/com/example/meepmeeptesting/autoTest2.java
+++ meepMeepTesting/src/main/java/com/example/meepmeeptesting/autoTest2.java
@@ -10,72 +10,59 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class autoTest2 {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
-
-        // Declare our first bot
-        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be blue
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
-        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(24, -61, Math.toRadians(90)))
-                        .strafeTo(new Vector2d(26,-59))
-                        .setTangent(Math.toRadians(180))
-                        .splineToLinearHeading(new Pose2d(-25,-15,Math.toRadians(0)),Math.toRadians(90))
-                        .waitSeconds(6)
-                        .lineToX(-26)
-                        .setTangent(Math.toRadians(268))
-                        .splineToConstantHeading(new Vector2d(56,-62),Math.toRadians(0))
-
-                .build());
-        RoadRunnerBotEntity bot2 = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeBlueDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-        bot2.runAction(bot2.getDrive().actionBuilder(new Pose2d(24,61,Math.toRadians(270)))
-                .splineToLinearHeading(new Pose2d(25,15,Math.toRadians(180)),Math.toRadians(180))
-                .waitSeconds(6)
-                .lineToX(26)
-                .setTangent(Math.toRadians(3))
-                .splineToConstantHeading(new Vector2d(-56,62),Math.toRadians(179))
-                .build());
-
+        MeepMeep meepMeep = new MeepMeep(600);
+//------------------------------------------------------------------------------------------------------------------------------------
+// **********BOTTOM LEFT************
         RoadRunnerBotEntity bot3 = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
                 .build();
 
-        bot3.runAction(bot3.getDrive().actionBuilder(new Pose2d(-24, -61, Math.toRadians(90)))
-                        .setTangent(Math.toRadians(85))
-                        .splineToLinearHeading(new Pose2d(-25,15,Math.toRadians(0)),Math.toRadians(25))
-                        .waitSeconds(6)
-                        .lineToX(-35)
-                        .setTangent(Math.toRadians(235))
-                        .splineToConstantHeading(new Vector2d(56,-62),Math.toRadians(0))
-                        .build());
-        RoadRunnerBotEntity bot4 = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeBlueDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
+        bot3.runAction(bot3.getDrive().actionBuilder(new Pose2d(-9, -63, Math.toRadians(90)))
+//                .strafeTo(new Vector2d(0, -52))
+//                //April Tag detect
+//                .waitSeconds(2)
+                .strafeTo(new Vector2d(-9,-35))
+                //Place Specimen
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(-48,-40))
+                //Pick up sample 1
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(-55,-55), Math.toRadians(225))
+                //Place sample 1
+                .waitSeconds(3)
+                .strafeToLinearHeading(new Vector2d(-58,-40), Math.toRadians(90))
+                //Pick up sample 2
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(-55,-55), Math.toRadians(225))
+                //Place sample 2
+                .waitSeconds(3)
+                .strafeToLinearHeading(new Vector2d(-54,-25), Math.toRadians(180))
+                //Pick up Sample 3
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(-55,-55), Math.toRadians(225))
+                //Place Sample 3
+                .waitSeconds(3)
+                .strafeToLinearHeading(new Vector2d(-23, -12), Math.toRadians(0))
+//                         .strafeTo(new Vector2d(-10,-35))
+//                         .waitSeconds(3)
+//                         .lineToY(-40)
+//                         .splineToConstantHeading(new Vector2d(-48,-40),Math.toRadians(90))
+//                        .waitSeconds(2)
+//                        .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(225)),Math.toRadians(180))
+//                        .waitSeconds(2)
+//                        .splineToLinearHeading(new Pose2d(-58,-40,Math.toRadians(90)),Math.toRadians(90))
+//                        .waitSeconds(2)
+//                        .splineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(225)),Math.toRadians(180))
 
-        bot4.runAction(bot4.getDrive().actionBuilder(new Pose2d(-24, 61, Math.toRadians(270)))
-                        .setTangent(Math.toRadians(350))
-                        .splineToLinearHeading(new Pose2d(25,-15,Math.toRadians(180)),Math.toRadians(180))
-                        .waitSeconds(6)
-                        .lineToX(34)
-                        .setTangent(45)
-                        .splineToConstantHeading(new Vector2d(-56,62),Math.toRadians(180))
                         .build());
-
+//------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 // Add both of our declared bot entities
-                .addEntity(myFirstBot)
-                .addEntity(bot2)
                 .addEntity(bot3)
-                .addEntity(bot4)
                 .start();
 
 
